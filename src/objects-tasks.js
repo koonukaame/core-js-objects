35f88ba45564c2b99d757b99813fc02545ccdc56
koonukaame <marialezhebokova@yandex.ru>
2025-02-25 19:32:45 +0300

    feat: solve sellTicket

--- src/objects-tasks.js
+++ src/objects-tasks.js
@@ -151,8 +151,26 @@ function makeWord(lettersObject) {
  *    sellTickets([25, 25, 50]) => true
  *    sellTickets([25, 100]) => false (The seller does not have enough money to give change.)
  */
-function sellTickets(/* queue */) {
-  throw new Error('Not implemented');
+function sellTickets(queue) {
+  if (queue.length === 0) return true;
+  let change = 0;
+  for (let i = 0; i < queue.length; i += 1) {
+    const bill = queue[i];
+    if (bill === 25) change += 25;
+    else if (bill === 50) {
+      if (change >= 25) {
+        change -= 25;
+        change += 50;
+      } else return false;
+    } else if (bill === 100) {
+      if (change >= 25 && change >= 75) {
+        change -= 50;
+        change -= 25;
+      } else if (change >= 75) change -= 75;
+      else return false;
+    }
+  }
+  return true;
 }
 
 /**
